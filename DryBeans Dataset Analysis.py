43a089ec8c749ac4b4dd41f55c1f80920e5405a3
Syed Anser <113206580+SyedAnser@users.noreply.github.com>
2023-12-05 11:54:49 +0530

    Delete DryBeans Dataset Analysis.py

--- DryBeans Dataset Analysis.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df=pd.read_excel("C:/Users/syeda/OneDrive/Desktop/Datasets/DryBeanDataset/Dry_Bean_Dataset.xlsx")
-sns.pairplot(df, hue='Class')
-
-plt.show()
